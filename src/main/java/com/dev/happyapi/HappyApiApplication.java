7a6c2d432c2cb73a9dc1c5fdfa6cf2ae0a7509e2
Jirlan Souza <jirlansouza08@gmail.com>
2023-07-01 15:56:38 -0400

    configurado springboot no projeto

---
+++ src/main/java/com/dev/happyapi/HappyApiApplication.java
@@ -0,0 +1,13 @@
+package com.dev.happyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyApiApplication.class, args);
+	}
+
+}
