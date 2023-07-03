464a27dea8d1743863328cce80affc296ff52564
Jirlan Souza <jirlansouza08@gmail.com>
2023-07-02 20:32:14 -0400

    resolvido o problema de `N+1` do `JPA` no method `findALl` no `OrphaanagesRepository`

--- src/main/java/com/dev/happyapi/orphanage/data/repositories/OrphanagesRepository.java
+++ src/main/java/com/dev/happyapi/orphanage/data/repositories/OrphanagesRepository.java
@@ -2,11 +2,16 @@ package com.dev.happyapi.orphanage.data.repositories;
 
 import com.dev.happyapi.orphanage.data.models.Orphanage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface OrphanagesRepository extends JpaRepository<Orphanage, UUID> {
     boolean existsByName(String name);
+
+    @Query(value = "from Orphanage o join fetch o.images")
+    List<Orphanage> findAll();
 }
