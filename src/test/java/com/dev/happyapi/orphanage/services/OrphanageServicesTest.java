17ef641b45c2177e9450b865ab7f2cb6ce965575
Jirlan Souza <jirlansouza08@gmail.com>
2023-08-07 21:03:45 -0400

    test: added tests in `OrphanageServicesTest` class

--- src/test/java/com/dev/happyapi/orphanage/services/OrphanageServicesTest.java
+++ src/test/java/com/dev/happyapi/orphanage/services/OrphanageServicesTest.java
@@ -1,6 +1,7 @@
 package com.dev.happyapi.orphanage.services;
 
 import com.dev.happyapi.orphanage.data.models.Orphanage;
+import com.dev.happyapi.orphanage.data.models.OrphanageImage;
 import com.dev.happyapi.orphanage.data.repositories.OrphanagesRepository;
 import com.dev.happyapi.orphanage.dtos.CreateOrphanageDto;
 import com.dev.happyapi.orphanage.exceptions.ExistsEntityException;
@@ -10,9 +11,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -25,6 +29,9 @@ class OrphanageServicesTest {
 
     @Mock
     OrphanageImageUploadService imageUploadService;
+
+    @Mock
+    MultipartFile imageFile;
 
     @InjectMocks
     OrphanageServices services;
@@ -50,6 +57,16 @@ class OrphanageServicesTest {
         assertEquals(result.getName(), dto.name());
     }
 
+    @Test
+    public void shouldNotCreateOrphanageImageWhenImageFileIsEmpty() {
+        CreateOrphanageDto dto = createOrphanageDto();
+
+        Mockito.when(imageUploadService.saveImages(Mockito.any())).thenReturn(Collections.emptyList());
+        services.createOrphanage(dto);
+
+        Mockito.verify(orphanagesRepository).save(Mockito.argThat(a -> a.getImages().size() == 0));
+    }
+
     private CreateOrphanageDto createOrphanageDto() {
         return new CreateOrphanageDto(
                 "Orphanage",
@@ -59,13 +76,15 @@ class OrphanageServicesTest {
                 15.158563,
                 "Orphanage test opening hours",
                 false,
-                Collections.emptyList()
+                List.of(imageFile)
         );
     }
 
     private Orphanage createOrphanage(CreateOrphanageDto dto) {
+        UUID id = UUID.randomUUID();
+
         return new Orphanage(
-                UUID.randomUUID(),
+                id,
                 dto.name(),
                 dto.about(),
                 dto.instructions(),
@@ -73,7 +92,20 @@ class OrphanageServicesTest {
                 dto.longitude(),
                 dto.opening_hours(),
                 dto.open_on_weekends(),
-                Collections.emptyList()
+                dto.images().stream().map(
+                        i -> createOrphanageImage(i, id)).collect(Collectors.toList()
+                )
+        );
+    }
+
+    private OrphanageImage createOrphanageImage(MultipartFile imageFile, UUID orphanageId) {
+        return new OrphanageImage(
+                UUID.randomUUID(),
+                orphanageId,
+                imageFile.getOriginalFilename(),
+                imageFile.getSize(),
+                imageFile.getContentType(),
+                "****"
         );
     }
 }
